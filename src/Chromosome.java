2bd81b08212aaca5567537c1a5e9e802eb9d1bf4
Linda <linda.latreider@web.de>
2016-01-18 13:33:58 +0100

    Chromosome Grundlagen
    Gerüst Interface und Klasse

---
+++ src/Chromosome.java
@@ -0,0 +1,25 @@
+/**
+ * Created by Linda on 18.01.2016.
+ */
+
+public class Chromosome implements IChromosome {
+
+    private int budget = 1500;
+    private int length = 150;
+
+    public String getChromosome(){
+        return null; //TODO
+
+    }
+
+    public boolean isValid(){
+        return true; //TODO
+
+    }
+
+    public int getFitness(){
+        return 0; //TODO
+
+    }
+
+}
---
+++ src/IChromosome.java
@@ -0,0 +1,12 @@
+/**
+ * Created by Linda on 18.01.2016.
+ */
+public interface IChromosome {
+
+    public String getChromosome();
+
+    public boolean isValid();
+
+    public int getFitness();
+
+}
