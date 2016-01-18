2b0bed21b73bf1a6841254edf7107edf7e3f154d
Bean900 <n.nikisch@gmx.de>
2016-01-18 13:04:49 +0100

    Enum angelegt

---
+++ src/Configuration.java
@@ -0,0 +1,8 @@
+/**
+ * Created by 9364290 on 18.01.16.
+ */
+public enum Configuration {
+
+    OnePoint,TwoPoint, KPoint, Uniform, Displacement, Exchange, Insertion, Inversion, Scramble, RoubletteWheel, Tournament;
+
+}
