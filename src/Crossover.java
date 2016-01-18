fa0d6580176b22e5df12f0a62a421f038075be92
Linda <linda.latreider@web.de>
2016-01-18 14:00:40 +0100

    Crossover Gerüst

---
+++ src/Crossover.java
@@ -0,0 +1,15 @@
+/**
+ * Created by Linda on 18.01.2016.
+ */
+public class Crossover implements ICrossover {
+
+    public Chromosome[] doCrossover(Chromosome parent1, Chromosome parent2){
+
+        Chromosome[] children = new Chromosome[2];
+        return children;
+        //TODO
+
+    }
+
+
+}
